28eaf1cf44cbec1f8264809ffd071575d52bd4ad
vise07cs <vikram7cs@gmail.com>
2025-04-21 19:22:39 +0530

    E01 nested structure challenges

--- App.js
+++ App.js
@@ -2,15 +2,35 @@
   const parent= React.createElement(
     "div",
     {id:"parent", className:"parent"},
-    React.createElement("h1", {id:"child1", }, " I am Child 1"),
-    React.createElement("h1", {id:"child2", }, "I am Child 2"),
-    React.createElement("h1", {id:"child3",}, "I am Child 3")
+    React.createElement("h1", {id:"child1", }, [
+      React.createElement("h1", {id:"child2", }, "I am Child 2"),
+      React.createElement("h2", {id:"child3",}, "I am Child 3")
+    ])
+    
   );
   const root=ReactDOM.createRoot(document.getElementById("root"));
   root.render(parent) // 
+  //React Element is actually a JavaScript object .which renders and converts into HTML element.
 
 
+
+// The above structure is a nested structure of React elements.
   // The above code creates a parent div with three child h1 elements using React's `createElement` method.
+  
+// The nested structuere is as follows:
+// ```html
+// <div id="parent" class="parent">
+//   <h1 id="child1">
+//     <h1 id="child2">I am Child 2</h1>
+//     <h2 id="child3">I am Child 3</h2>
+//   </h1>
+// </div>
+// ```
+// The above code creates a parent div with three child h1 elements using React's `createElement` method.
+// However , the react code becomes a bit complex when we have to create a nested structure of elements.
+// So, we can use JSX syntax to create a nested structure of elements easily.
+
+
   
   
   
