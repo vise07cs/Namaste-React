375e8cbc4d3469876e219b6d2035989d96c37497
vise07cs <vikram7cs@gmail.com>
2025-04-30 22:50:08 +0530

    E03

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-  const parent= React.createElement(
-    "div",
-    {id:"parent", className:"parent"},
-    React.createElement("h1", {id:"child1", }, "Hello World!"),
-    
-      
-    )
-    
-  
-  const root=ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parent) // 
-
-  
-  
-
  
+
+//JSX
+
+
+const Title =(
+  <h1 className="head">
+    Title React Element
+  </h1>
+)
+
+// React Component  --> Everything is a component in react
+
+
+const num=100;
+// const data =api.getData(); // api is a function that returns data
+const HeadingComponent = () => (
+  <div id="container">
+    {Title}
+    <h1 className="heading">Namste React HeadingFunctional Component </h1>
+    
+    <h2>{num}</h2>
+    {/* {data} */}
+
+  </div>
+ 
+    
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
+
+
+
